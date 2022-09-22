a1a212011172da0e8c4105789aefc41b1d9a1ada
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-22 13:24:07 +0530

    Add project files.

---
+++ Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, a;
+            Console.WriteLine("Enter radius of circle");
+            r=double.Parse(Console.ReadLine());
+            a = 3.14 * r * r;
+            Console.WriteLine(a + "");
+
+            
+        }
+    }
+}
